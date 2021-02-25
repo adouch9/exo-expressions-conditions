82a7f8f00bd81bbed3592f277553fdeabd93968d
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-25 16:43:01 +0100

    Welcome

---
+++ welcome.js
@@ -0,0 +1,18 @@
+let firstname = "Bob";
+let firstname2 = "Alice";
+let genderMen = true;
+let genderWomen = false;
+let gender= true
+
+
+
+gender =(gender === genderMen && firstname === "Bob" ) ? ' Bonjour Bob, vous êtes entré sur le chan vote' : ' Bonjour Alice, vous êtes entrée sur le chan not vote'
+
+console.log(gender)
+
+
+
+
+
+
+
